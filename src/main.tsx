57c0e136b7e51822ff1e24468553901faa849e2f
RhythmicDias <stephen.dias@gmail.com>
2026-07-05 23:44:58 +0400

    Disable default browser right-click context menu globally

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// Disable default right-click context menu
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
